eb567c813c9f30ed7b727ad720d7f1ee7b1f73f3
Noah Shorter <shorter.noah@gmail.com>
2021-04-12 13:44:34 -0700

    added skeliton form to public

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,11 +1,12 @@
 <template>
     <div class="Contact">
         <h1>This is the Contact page</h1>
-        <form name="contact" method="POST" netlify>
-          <input required name="Name" type="text" placeholder="Name"/>
-          <input required name="Email" type="email" placeholder="Email"/>
-          <textarea name="Message" placeholder="Message"/>
-          <button type="submit">Send</button>
+        <form name="contact" method="POST" data-netlify="true">
+          <input type="hidden" name="form-name" value="contact"/>
+          <p><input required name="Name" type="text" placeholder="Name"/></p>
+          <p><input required name="Email" type="email" placeholder="Email"/></p>
+          <p><textarea name="Message" placeholder="Message"/></p>
+          <p><button type="submit">Send</button></p>
         </form>
     </div>
 </template>
