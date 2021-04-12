21e93d8d8e9de49788beee763fafe779bc642655
Noah Shorter <shorter.noah@gmail.com>
2021-04-12 13:13:54 -0700

    added simple contact form

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,6 +1,12 @@
 <template>
     <div class="Contact">
         <h1>This is the Contact page</h1>
+        <form name="contact" method="POST" netlify>
+          <input required name="Name" type="text" placeholder="Name"/>
+          <input required name="Email" type="email" placeholder="Email"/>
+          <textarea name="Message" placeholder="Message"/>
+          <button type="submit">Send</button>
+        </form>
     </div>
 </template>
 
