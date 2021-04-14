3e1494845804e56f99fdc260a3f2b6a794c01ac0
Noah Shorter <shorter.noah@gmail.com>
2021-04-13 20:15:55 -0700

    fixed spacing bug

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -7,7 +7,7 @@
           <p><label>Name *</label><input required name="Name" type="text"/></p>
           <p><label>Email Address *</label><input required name="Email" type="email"/></p>
           <p><label>Message</label><textarea name="Message"/></p>
-          <p><button type="submit">Send</button></p>
+          <p><button type="submit" class="subButton">Send</button></p>
         </form>
         <div class="digits"><h4 class="labels">Email</h4><p>shorter.noah@gmail.com</p></div>
         <div class="digits"><h4 class="labels">Phone Number</h4><p>541-561-8278</p></div>
@@ -30,23 +30,22 @@ export default {
   margin-right: auto;
 }
 
-input, textarea {
+.Contact input, textarea {
   width: 100%;
 }
 
-label {
+.Contact label {
   width: 100%;
   text-align: left;
 }
 
-button {
+.Contact .subButton {
   background: rgb(255,136,1);
   background: linear-gradient(90deg, rgba(255,136,0,1) 0%, rgba(252,192,69,1) 100%);
   border: none;
   width: 100px;
   height: 40px;
-  justify-self: left;
-  margin-right: 100%;
+  margin-right: calc(100% - 100px);
   font-weight: 700;
   border-radius: 2px;
 }
