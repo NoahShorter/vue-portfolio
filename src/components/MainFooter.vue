b56d75a8c97e65619c4a8a3bf1f7c0ef8562d3be
Noah Shorter <shorter.noah@gmail.com>
2021-04-14 15:24:56 -0700

    Worked on the footer. Simple design

--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -1,15 +1,70 @@
 <template>
     <div class="footer">
-        <p></p>
+      <ul class="socialMedia">
+        <li><a href="https://www.facebook.com/noah.shorter"><img src="../assets/social-1_square-facebook_orange.svg"/></a></li>
+        <li><a href="https://www.linkedin.com/in/noah-shorter-6830201b6"><img src="../assets/social-1_square-linkedin_orange.svg"/></a></li>
+        <li><a href="https://www.github.com/NoahShorter"><img src="../assets/social-1_square-github_orange.svg"/></a></li>
+        <li><a href="mailto: shorter.noah@gmail.com"><img src="../assets/social-1_square-google-plus_orange.svg"/></a></li>
+      </ul>
+      <ul class="routerMap">
+        <li><router-link class="link" to="/">Home</router-link></li>
+        <li> | </li>
+        <li><router-link class="link" to="/Projects">Projects</router-link></li>
+        <li> | </li>
+        <li><router-link class="link" to="/Contact">Contact</router-link></li>
+      </ul>
     </div>
 </template>
 
+
 <style>
+* {
+  margin: 0;
+  padding: 0;
+}
+
 .footer {
   flex-shrink: 0;
   background: rgb(36,31,54);
   background: linear-gradient(83deg, rgba(36,31,54,1) 0%, rgba(29,24,46,1) 65%);
   color: white;
-  padding: 20px;
 }
+
+.socialMedia {
+  margin: 0;
+  margin-top: 20px;
+  margin-bottom: 10px;
+}
+
+.socialMedia li {
+  display: inline;
+  margin-left: 10px;
+  margin-right: 10px;
+}
+
+.socialMedia img{
+  width: 32px;
+  height: 32px;
+  color: rgb(255,136,1);
+  color: linear-gradient(90deg, rgba(255,136,0,1) 0%, rgba(252,192,69,1) 100%);
+}
+
+.routerMap {
+  margin: 0;
+  margin-top: 10px;
+  margin-bottom: 20px;
+}
+
+.routerMap li{
+  display: inline;
+}
+
+.link {
+  color: white;
+}
+
+.link:hover {
+  color: white;
+}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router.js'
+import SvgFiller from 'vue-svg-filler'
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 Vue.config.productionTip = false
 
+Vue.component('svg-filler', SvgFiller)
+
 new Vue({
   render: h => h(App),
-  router
+  router,
 }).$mount('#app')
